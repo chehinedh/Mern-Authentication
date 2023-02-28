7eda22a75063639695c6f7fae064c41c7b8083d3
chehinedh <chehine.dhemaied@esprit.tn>
2023-02-28 23:03:26 +0100

    🎉 Initialize Auth Mern Project

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require("../models/userModel");
+
+// login user
+const loginUser = async (req, res) => {
+  res.json({ msg: "login user" });
+};
+
+//signup user
+const signupUser = async (req, res) => {
+  res.json({ msg: "signup user" });
+};
+
+module.exports = {
+  loginUser,
+  signupUser,
+};
