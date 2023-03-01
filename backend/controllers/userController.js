e6056fbb8b1c7d222f55f2d5d66a303c95c08fb6
chehinedh <chehine.dhemaied@esprit.tn>
2023-03-01 22:06:02 +0100

    ✨ Signing Tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' } )
+}
 
 // login user
 const loginUser = async (req, res) => {
@@ -11,7 +16,11 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.create({ email, password });
-    res.status(200).json({ email , user });
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({ email , token });
   } catch (error) {
     res.status(400).json({error: error.message})
   }
