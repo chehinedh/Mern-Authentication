84232449a244043c2c89a65a4c7a750d6165549e
chehinedh <chehine.dhemaied@esprit.tn>
2023-03-01 21:33:21 +0100

    ⚡️ Signing Up & Hashing Passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,15 @@ const loginUser = async (req, res) => {
 
 //signup user
 const signupUser = async (req, res) => {
+  const {email, password} = req.body 
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(200).json({ email , user });
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+
   res.json({ msg: "signup user" });
 };
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,4 +14,21 @@ const userSchema = new Schema({
     }
 })
 
+// static signup method 
+userSchema.statics.signup = async function(email, password) {
+    
+    const exists = await this.findOne({ email })
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({ email, password: hash});
+
+    return user;
+
+};
+
 module.exports = mongoose.model('User', userSchema)
